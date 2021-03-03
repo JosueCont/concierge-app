4045ee00d7c99c46185cb698ff496caa89df3e94
hiuman <jasson.acea@hiumanlab.com>
2021-03-03 15:36:20 -0600

    loading

--- app/components/modal/loading.js
+++ app/components/modal/loading.js
@@ -13,8 +13,8 @@ import { connect } from "react-redux";
 
 const ModalLoading = (props) => {
   return (
-    <View style={{ backgroundColor: "green" }}>
-      <Modal animationType="slide" transparent={true} visible={props.visible}>
+    <View>
+      <Modal transparent={true} visible={props.visible}>
         <View style={styles.centeredView}>
           <Image
             style={{ width: 100, height: 100 }}
@@ -34,62 +34,6 @@ const styles = {
     backgroundColor: "rgba(0,0,0,0.53)",
     justifyContent: "center",
     alignItems: "center",
-  },
-  fbBtn: {
-    backgroundColor: Colors.purple,
-    width: height / 3,
-    height: 40,
-    borderRadius: 10,
-    marginTop: 30,
-    paddingHorizontal: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row",
-  },
-  fbText: {
-    color: Colors.white,
-    fontSize: 14,
-  },
-  modalView: {
-    margin: 20,
-    width: "80%",
-    backgroundColor: Colors.orange,
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  openButton: {
-    backgroundColor: "#F194FF",
-    borderRadius: 10,
-    //padding: 10,
-    //elevation: 2
-  },
-  textStyle: {
-    fontSize: 12,
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-    fontFamily: "Cabin-Regular",
-    color: Colors.purple,
-  },
-  modalTitle: {
-    marginBottom: 5,
-    textAlign: "center",
-    fontFamily: "Cabin-Bold",
-    fontSize: 20,
-    color: Colors.purple,
   },
 };
 
