e65eb09cea1d3a98804e8ad93a0b97942274c3da
hiuman <jasson.acea@hiumanlab.com>
2021-07-20 19:27:22 -0500

    home screen

--- app/components/modal/LoadingGlobal.js
+++ app/components/modal/LoadingGlobal.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { Modal, View, Image, Text } from "react-native";
 import { connect } from "react-redux";
-import gif from "../../../assets/Login_concierge.gif"
+import gif from "../../../assets/Login_concierge.gif";
 
 const ModalLoadingGlobal = (props) => {
   return (
-    <View style={{flex:1,height:'100%'}}>
+    <View style={{ flex: 1, height: "100%", backgroundColor: "green" }}>
       <Modal transparent={true} visible={props.visible}>
         <View style={styles.centeredView}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            fullScreen
-            source={gif}
-          />
+          <Image style={{ width: 100, height: 100 }} fullScreen source={gif} />
           <Text style={{ color: "#fff" }}>{props.text}...</Text>
         </View>
       </Modal>
@@ -26,7 +22,6 @@ const styles = {
     backgroundColor: "rgba(0,0,0,0.7)",
     justifyContent: "center",
     alignItems: "center",
-
   },
 };
 
--- app/screens/HomeUserScreen.js
+++ app/screens/HomeUserScreen.js
@@ -1,183 +1,183 @@
-import {View, ScrollView, Text, StatusBar} from "react-native";
+import { View, ScrollView, Text, StatusBar } from "react-native";
 import ToolbarGeneric from "../components/ToolbarComponent/ToolbarGeneric";
-import React, {useEffect, useState} from "react";
-import {connect} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 import ComunicationCard from "../components/ComponentCards/ComunicationCard";
-import {Colors} from "../utils/colors";
+import { Colors } from "../utils/colors";
 import ApiApp from "../utils/ApiApp";
 import LoadingGlobal from "../components/modal/LoadingGlobal";
-import {getProfile, logOutAction} from "../redux/userDuck";
-import {registerForPushNotificationsAsync} from "../utils/functions";
+import { getProfile, logOutAction } from "../redux/userDuck";
+import { registerForPushNotificationsAsync } from "../utils/functions";
 import ModalNews from "../components/modal/ModalNews";
 
 const HomeUserScreen = (props) => {
-    const [comunications, setComunications] = useState([]);
-    const [modalLoading, setModalLoading] = useState(true);
-    const [modalNews, setModalNews] = useState(false);
-    const [modalItem, setModalItem] = useState({});
-    const [refresh, setRefresh] = useState(false);
+  const [comunications, setComunications] = useState([]);
+  const [modalLoading, setModalLoading] = useState(true);
+  const [modalNews, setModalNews] = useState(false);
+  const [modalItem, setModalItem] = useState({});
+  const [refresh, setRefresh] = useState(false);
 
-    const clickAction = () => {
-        props.navigation.toggleDrawer();
-    };
+  const clickAction = () => {
+    props.navigation.toggleDrawer();
+  };
 
+  const goHome = () => {
+    props.navigation.navigate("Home");
+  };
 
-    const goHome = () => {
-        props.navigation.navigate("Home");
-    };
+  const clickProfile = () => {
+    props.navigation.navigate("ProfileScreen");
+  };
 
-    const clickProfile = () => {
-        props.navigation.navigate("ProfileScreen");
-    };
+  useEffect(() => {
+    props.getProfile(props.user).then(async (response) => {
+      const device_id = await registerForPushNotificationsAsync();
+      if (response.id) {
+        let data = {
+          person: response.id,
+          mobile_os: Platform.OS == "android" ? 1 : Platform.OS == "ios" && 2,
+          device_id: device_id,
+        };
+        if (device_id && device_id != "") ApiApp.userDevice(data);
+        getComunication();
+      } else if (response == "Error") {
+        setTimeout(() => {
+          setModalLoading(false);
+          props.logOutAction();
+        }, 1000);
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        props.getProfile(props.user).then(async (response) => {
-            const device_id = await registerForPushNotificationsAsync();
-            if (response.id) {
-                let data = {
-                    person: response.id,
-                    mobile_os: Platform.OS == "android" ? 1 : Platform.OS == "ios" && 2,
-                    device_id: device_id,
-                };
-                if (device_id && device_id != "") ApiApp.userDevice(data);
-                getComunication();
-            } else if (response == "Error") {
-                setTimeout(() => {
-                    setModalLoading(false);
-                    props.logOutAction();
-                }, 1000);
-            }
-        });
-    }, []);
+  useEffect(() => {
+    getComunication();
+  }, [props.user.userProfile]);
 
-    // useEffect(() => {
-    //   getComunication();
-    // }, [props.user.userProfile]);
+  const getComunication = async (value) => {
+    setModalLoading(true);
+    try {
+      let response = await ApiApp.getComunication(props.user.userProfile.id);
+      setComunications(response.data.results);
+      setTimeout(() => {
+        if (value && value == "refrescar") setRefresh(false);
+        setModalLoading(false);
+      }, 1500);
+    } catch (err) {
+      console.log(err.response);
+      setTimeout(() => {
+        if (value && value == "refrescar") setRefresh(false);
+        setModalLoading(false);
+      }, 1500);
+    }
+  };
 
-    const getComunication = async (value) => {
-        setModalLoading(true);
-        try {
-            let response = await ApiApp.getComunication(props.user.userProfile.id);
-            setComunications(response.data.results);
-            setTimeout(() => {
-                if (value && value == "refrescar") setRefresh(false);
-                setModalLoading(false);
-            }, 1500);
-        } catch (err) {
-            console.log(err.response)
-            setTimeout(() => {
-                if (value && value == "refrescar") setRefresh(false);
-                setModalLoading(false);
-            }, 1500);
-        }
-    };
-
-    const headerList = () => {
-        return (
-            <View
-                style={{
-                    zIndex: 0,
-                    paddingHorizontal: 22,
-                    marginBottom: 20,
-                }}
-            >
-                <View style={{alignItems: "center"}}>
-                    <View
-                        style={{
-                            flexDirection: "row",
-                            alignItems: "center",
-                            marginTop: 20,
-                        }}
-                    >
-                        <Text
-                            style={{
-                                fontFamily: "Cabin-Regular",
-                                color: Colors.bluetitle,
-                                fontSize: 28,
-                            }}
-                        >
-                            Hola
-                        </Text>
-                        <Text
-                            style={{
-                                fontFamily: "Cabin-Bold",
-                                color: Colors.bluetitle,
-                                fontSize: 28,
-                                marginLeft: 5,
-                            }}
-                        >
-                            {props.user.userProfile && props.user.userProfile.first_name}
-                        </Text>
-                    </View>
-                    <Text
-                        style={{
-                            fontFamily: "Cabin-Regular",
-                            color: Colors.bluetitle,
-                            fontSize: 30,
-                            textAlign: "center",
-                        }}
-                    >
-                        Te mantenemos informado
-                    </Text>
-                </View>
-            </View>
-        );
-    };
-
-    const openModalNews = (value) => {
-        modalNews ? setModalNews(false) : setModalNews(true);
-        setModalItem(value);
-    };
-    const changeStatus = (value) => {
-        comunications[value.index].is_red = true;
-        setModalNews(false);
-    };
-
+  const headerList = () => {
     return (
-        <>
-            <View
-                style={{
-                    height: "100%",
-                    zIndex: 1,
-                }}
+      <View
+        style={{
+          zIndex: 0,
+          paddingHorizontal: 22,
+          marginBottom: 20,
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginTop: 20,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: "Cabin-Regular",
+                color: Colors.bluetitle,
+                fontSize: 28,
+              }}
             >
-                <StatusBar
-                    barStyle="dark-content"
-                    backgroundColor="rgba(1,1,1,0)"
-                    translucent={true}
-                />
-                <ToolbarGeneric
-                    clickAction={clickAction}
-                    nameToolbar={"Menú"}
-                    type={2}
-                    clickProfile={clickProfile}
-                    goHome={goHome}
-                />
-
-                <ComunicationCard
-                    cards={comunications}
-                    props={props}
-                    headerList={headerList}
-                    refresh={(value) => {
-                        getComunication(value), setRefresh(true);
-                    }}
-                    modalNews={(value) => openModalNews(value)}
-                    refreshing={refresh}
-                />
-                <LoadingGlobal visible={modalLoading} text={"Cargando"}/>
-            </View>
-            <ModalNews
-                visible={modalNews}
-                data={modalItem}
-                setVisible={(value) => changeStatus(value)}
-            />
-        </>
+              Hola
+            </Text>
+            <Text
+              style={{
+                fontFamily: "Cabin-Bold",
+                color: Colors.bluetitle,
+                fontSize: 28,
+                marginLeft: 5,
+              }}
+            >
+              {props.user.userProfile && props.user.userProfile.first_name}
+            </Text>
+          </View>
+          <Text
+            style={{
+              fontFamily: "Cabin-Regular",
+              color: Colors.bluetitle,
+              fontSize: 30,
+              textAlign: "center",
+            }}
+          >
+            Te mantenemos informado
+          </Text>
+        </View>
+      </View>
     );
+  };
+
+  const openModalNews = (value) => {
+    modalNews ? setModalNews(false) : setModalNews(true);
+    setModalItem(value);
+  };
+  const changeStatus = (value) => {
+    comunications[value.index].is_read = true;
+    setModalNews(false);
+  };
+
+  return (
+    <>
+      <View
+        style={{
+          backgroundColor: "red",
+          height: "100%",
+          zIndex: 1,
+        }}
+      >
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor="rgba(1,1,1,0)"
+          translucent={true}
+        />
+        <ToolbarGeneric
+          clickAction={clickAction}
+          nameToolbar={"Menú"}
+          type={2}
+          clickProfile={clickProfile}
+          goHome={goHome}
+        />
+
+        <ComunicationCard
+          cards={comunications}
+          props={props}
+          headerList={headerList}
+          refresh={(value) => {
+            getComunication(value), setRefresh(true);
+          }}
+          modalNews={(value) => openModalNews(value)}
+          refreshing={refresh}
+        />
+      </View>
+      <LoadingGlobal visible={modalLoading} text={"Cargando"} />
+      <ModalNews
+        visible={modalNews}
+        data={modalItem}
+        setVisible={(value) => changeStatus(value)}
+      />
+    </>
+  );
 };
 
 const mapState = (state) => {
-    return {
-        user: state.user,
-    };
+  return {
+    user: state.user,
+  };
 };
-export default connect(mapState, {getProfile, logOutAction})(HomeUserScreen);
+export default connect(mapState, { getProfile, logOutAction })(HomeUserScreen);
